1c73e9796196b494a23b5a58cba8521d7f22714d
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2026-02-11 17:47:23 -0500

    Add command to run browser with specific flags

--- xcloud-setup.sh
+++ xcloud-setup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 flatpak --user override --filesystem=/run/udev:ro com.microsoft.Edge
+/home/deck/browser.sh --flag-switches-begin --enable-features=msOverlayScrollbarWinStyle -flag-switches-end
