ae3e676687bb2dd693e53b2cbe08922900d94aee
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2026-02-11 18:59:48 -0500

    Modify xcloud.sh to handle optional argument
    Add conditional shortcut handling for launch argument

--- xcloud.sh
+++ xcloud.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-/home/deck/kiosk.sh xbox.com/play/launch/$1
+if [ -n "$1" ]; then
+    shortcut="launch/$1"
+else
+    shortcut=""
+fi
+
+echo $shortcut
+
+/home/deck/kiosk.sh xbox.com/play/${shortcut}
