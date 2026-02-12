08da47f5df993acc8a4f802e2c2eebc92dc93ba9
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2026-02-11 19:00:31 -0500

    Add conditional shortcut handling in xhome.sh

--- xhome.sh
+++ xhome.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-/home/deck/kiosk.sh xbox.com/play/consoles/launch/$1
+
+if [ -n "$1" ]; then
+    shortcut="launch/$1"
+else
+    shortcut=""
+fi
+
+/home/deck/kiosk.sh xbox.com/play/consoles/${shortcut}
